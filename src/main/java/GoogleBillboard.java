2c58a3757d32eb11a187e2d5e2187e3256128882
mrchanr <42151767+mrchanr@users.noreply.github.com>
2024-01-22 14:33:59 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -7,16 +7,17 @@ public class GoogleBillboard {
         public int count= 0;
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
+		setup();
+	}
 	
-public void setup()  
+public static void setup()  
 {            
   for(int i=0; i<e.length(); i++){
     digits = e.substring(i,i+10);
     run= Double.parseDouble(digits);
     if(isPrime(run)==true){
-      return run;
-      break;
+      System.out.println(run);
+	//break; Dont need to break if you are already returning, creates an unreachable statement (bad
 } 
   }
 for(int i=0; i< e.length();i++){
